d208554b5bcee09200cfaf67286e021f9e0b8721
Jenish Talaviya <97280151+jenish4520@users.noreply.github.com>
2022-05-21 15:30:27 +0530

    Create Chocolate_Distribution_Problem.cpp

---
+++ 2022/MAY-2022/21-MAY-2022/Chocolate_Distribution_Problem.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    long long minn=LLONG_MAX;
+    sort(a.begin(),a.end());
+    for(long long i=0,j=m-1;j<n;i++,j++){
+        minn=min(minn,(a[j]-a[i]));
+    }
+    return minn;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
