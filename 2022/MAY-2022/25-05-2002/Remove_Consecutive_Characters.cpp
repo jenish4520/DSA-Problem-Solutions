65463a0959275cdfdf1335c2eb2ee726f2058c17
Jenish Talaviya <97280151+jenish4520@users.noreply.github.com>
2022-05-25 18:41:51 +0530

    Create Remove_Consecutive_Characters.cpp

---
+++ 2022/MAY-2022/25-05-2002/Remove_Consecutive_Characters.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+        string ans="";
+        for(int i=0;i<S.length();i++){
+            ans+=S[i];
+            while(S[i]==S[i+1]){
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+  // } Driver Code Ends
