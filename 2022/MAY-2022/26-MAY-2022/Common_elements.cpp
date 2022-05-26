eb878290905a5f89674ae042a3b584b7ceb9ee06
Jenish Talaviya <97280151+jenish4520@users.noreply.github.com>
2022-05-26 17:52:07 +0530

    Create Common_elements.cpp

---
+++ 2022/MAY-2022/26-MAY-2022/Common_elements.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            int i=0,j=0,k=0;
+            set<int>ans;
+            //code here.
+            for(int i=0;i<n1;i++){
+                while(B[j]<A[i] && j<n2) j++;
+                if(B[j]==A[i]){
+                    while(C[k]<A[i] && k<n3) k++;
+                    if(C[k]==A[i]){
+                        ans.insert(A[i]);
+                    }
+                }
+            }
+            vector<int>ansm(ans.begin(),ans.end());
+            return ansm;
+        }
+
+};
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}  // } Driver Code Ends
