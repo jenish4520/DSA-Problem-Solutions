8455f9a5ad15024503b5c48b2c0c5009110eeea3
Jenish Talaviya <97280151+jenish4520@users.noreply.github.com>
2022-05-26 15:51:56 +0530

    Create Keypad_Sequence.cpp

---
+++ 2022/MAY-2022/26-MAY-2022/Keypad_Sequence.cpp
@@ -0,0 +1,34 @@
+// C++ implementation to convert a
+// sentence into its equivalent
+// mobile numeric keypad sequence
+#include <bits/stdc++.h>
+using namespace std;
+string printSequence(string input);
+// Driver function
+int main()
+{
+	int t;
+	cin>>t;
+	cin.ignore();
+	while(t--)
+    {
+        string s;
+        getline(cin,s);
+        cout<<printSequence(s)<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+string printSequence(string S)
+{
+    //code here.
+    string arr[26]={"2","22","222","3","33","333","4","44","444","5","55","555","6","66","666","7","77","777","7777","8","88","888","9","99","999","9999"};
+    string ans="";
+    for(int i=0;i<S.length();i++){
+        if(S[i]==' ') ans+='0';
+        ans+=arr[S[i]-65];
+    }
+    return ans;
+}
