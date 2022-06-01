23c56f285bf627ea431993cb41b843ddf5e6f33d
Jenish Talaviya <97280151+jenish4520@users.noreply.github.com>
2022-06-01 09:20:45 +0530

    Create Search Insert Position.cpp

---
+++ 2022/June -2022/1-June-2022/Search Insert Position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size();
+        if(nums[0]>target) return 0;
+        if(nums[nums.size()-1]<target) return nums.size();
+        
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(target>nums[mid]) start=mid+1;
+            else if(target<nums[mid]) end=mid-1;
+            else return mid;
+        }
+        return start;
+    }
+};
